808e1736ae3c24ef1d0d5f8d767a0034b85a9c19
bocattelan <bocattelan@gmail.com>
2019-01-10 09:46:13 +0100

    Forgot to push the reportEmptyUsers method. Also, reporting now uses only id. Due to users being able to change the screen name variable, it sometimes made reporting unsuccessful

--- reportEmptyUsers.py
+++ reportEmptyUsers.py
@@ -4,23 +4,28 @@ import time
 import tweepy
 
 
-def reportBots(TARGET_USER, twitter_api, probabilityCut):
+# Due to limitations from twitter, can only report 20 accounts before having to wait
+def reportEmptyUsers(TARGET_USER, twitter_api):
     conn = sqlite3.connect('data/database.db')
     c = conn.cursor()
+    # Get all users where the universal score is -1 (no timeline) and was not reported (NULL)
+    # also filter out accounts known to have been deleted
+    users = c.execute(
+        'SELECT * FROM {} WHERE capUniversal=-1 AND (reported IS NULL AND (lastCheck!=0 or lastCheck IS NULL))'.format(
+            TARGET_USER)).fetchall()
 
-    users = c.execute('SELECT * FROM {} WHERE capUniversal >= ? AND (reported IS NULL AND (lastCheck!=0 or lastCheck IS NULL))'.format(
-            TARGET_USER), [probabilityCut]).fetchall()
-    print("Collected " + len(users).__str__() + " possible bots")
     for user in users:
         try:
+            # userTest = twitter_api.get_user(screen_name=user[1])
             twitter_api.report_spam(user_id=user[0])
             c.execute("UPDATE {} SET reported = ?, lastCheck = ? WHERE userId = ? AND userName = ?".format(TARGET_USER),
                       [1, time.time(), user[0], user[1]])
             conn.commit()
-            print("Reported user " + user[1] + " with probability " + user[3].__str__())
+            print("Reported user " + user[1] + " successfully")
         except tweepy.TweepError as error:
             if error.__str__() == "[{'code': 205, 'message': 'You are over the limit for spam reports.'}]":
                 print("Spam reporting limit reached")
+                # wait 30 minutes
                 return 205
             else:
                 print("Else: " + error.__str__())
